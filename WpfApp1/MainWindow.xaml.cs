8f05fe3dfb4cd7ac45a9dcb3f1df492279617afd
Zarcez <55078381+Zarcez@users.noreply.github.com>
2022-07-08 12:33:25 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int diceRoll = 0;
+        private Random rd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int dice = 0;
+            if (dice4.IsChecked == true)
+            {
+                dice = 4;    
+            }
+            else if (dice6.IsChecked == true)
+            {
+                dice = 6;
+            }
+            diceRolls.Text = "You have rolled "+ ++diceRoll +" times";
+            int result = roll(dice);
+            numberDisplay.Text = ""+result;
+
+        }
+
+        private int roll (int dice)
+        {
+            return rd.Next(1, dice+1);
+        }
+    }
+}
